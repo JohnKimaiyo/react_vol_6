66c5fdc4f7a646fe21f6bd89ec70848b09978d0c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 19:08:30 +0300

    tite prop called

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Greet from './component/Greet';
-import Welcome from './component/Welcome';
-import Hello from './component/Hello'
+import "./App.css";
+import Greet from "./component/Greet";
+import Welcome from "./component/Welcome";
+import Hello from "./component/Hello";
+import Title from "./component/Title";
 function App() {
   return (
     <div className="App">
       <Greet />
-      <Welcome/>
-      <Hello/>
+      <Welcome />
+      <Hello />
+      <Title name="Frontend Developer" />
+      <Title name="Mobile Developer" />
+      <Title name="Solidity Developer" />
     </div>
   );
 }
